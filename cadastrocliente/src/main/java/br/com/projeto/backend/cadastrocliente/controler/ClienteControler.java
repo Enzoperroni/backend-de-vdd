7ee7ad66f77c4233eb7bf83e4bc747023966f56a
ENZO MENEZES PERRONI <202201037598@alunos.ibmec.edu.br>
2024-02-29 09:02:59 -0300

    teste 2

--- cadastrocliente/src/main/java/br/com/projeto/backend/cadastrocliente/controler/ClienteControler.java
+++ cadastrocliente/src/main/java/br/com/projeto/backend/cadastrocliente/controler/ClienteControler.java
@@ -4,6 +4,8 @@ import br.com.projeto.backend.cadastrocliente.model.Cliente;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import java.util.ArrayList;
@@ -11,19 +13,42 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("/cliente")
 public class ClienteControler {
+
+    private static ArrayList<Cliente> repositorio = new ArrayList<>();
+
     @GetMapping("/listar")
     public String listarCliente(Model model){
-        ArrayList<Cliente>lista = new ArrayList<Cliente>();
-        Cliente cliente = new Cliente();
-        cliente.setNome("Enzo Perroni");
-        cliente.setEmail("qqcoisa@gmail.com");
-        cliente.setCpf("111.111.111-11");
-        cliente.setDataNascimento("01/01/2001");
 
-        lista.add(cliente);
-
-        model.addAttribute("listaCliente",lista);
+        model.addAttribute("listaCliente",repositorio);
 
         return "listar-cliente";
     }
+
+    @GetMapping("/adicionar")
+    public String adicionar(Cliente cliente){
+        return "salvar";
+    }
+    @PostMapping("/salvar")
+    public String salvar(Cliente cliente){
+        int id =1;
+        if (repositorio.size()>0){
+            Cliente ultimo = repositorio.get(repositorio.size() -1);
+            id = ultimo.getId() +1;
+        }
+        cliente.setId(id);
+        repositorio.add(cliente);
+        return "redirect:/cliente/listar";
+    }
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id")int id){
+        Cliente clienteASerExcluir = null;
+        for (Cliente item : repositorio){
+            if(item.getId()==id){
+                clienteASerExcluir = item;
+            }
+        }
+        repositorio.remove(clienteASerExcluir);
+        return "redirect:/cliente/listar";
+    }
+
 }
